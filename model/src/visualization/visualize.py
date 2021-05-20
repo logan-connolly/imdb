c0ba68481efa5641d656b0b47d2bb05ddc6efa94
Logan Connolly <logan.connolly@protonmail.com>
2021-05-20 23:16:55 +0200

    feat(visualize): add scatter plots of numeric features

--- model/src/visualization/visualize.py
+++ model/src/visualization/visualize.py
@@ -2,29 +2,39 @@ from functools import partial
 from typing import Callable
 
 import pandas as pd
-from seaborn import FacetGrid, displot
+import seaborn as sns
 
 from src.data.io import read_processed_data
 from src.data.paths import FIGPATH
 
-SeabornPlot = Callable[..., FacetGrid]
+SeabornPlot = Callable[..., sns.FacetGrid]
 
 
-def create_fig(df: pd.DataFrame, plt_fn: SeabornPlot, name: str, **kwargs) -> None:
+def plot_fig(df: pd.DataFrame, plt_fn: SeabornPlot, name: str, **kwargs) -> None:
     """Create plot figure and write out to reports/figures"""
-    fig = plt_fn(df, **kwargs)
+    fig = plt_fn(data=df, **kwargs)
     fig_path = FIGPATH / f"{plt_fn.__name__}_{name}.svg"
     fig.savefig(fig_path, format="svg")
 
 
 def generate_hists(df: pd.DataFrame) -> None:
     """Generate histogram plots for numeric columns"""
-    create_hist = partial(create_fig, df=df, plt_fn=displot, kind="hist")
+    create_hist = partial(plot_fig, df=df, plt_fn=sns.displot, kind="hist")
     create_hist(name="runtime", x="runtimeMinutes")
     create_hist(name="ratings", x="averageRating")
     create_hist(name="year", x="startYear")
 
 
+def generate_scatter_plots(df: pd.DataFrame) -> None:
+    """Generate scatter plots for numeric columns"""
+    create_scatter = partial(plot_fig, df=df, plt_fn=sns.relplot, kind="scatter")
+    create_scatter(name="runtime", x="runtimeMinutes", y="averageRating")
+    create_scatter(name="year", x="startYear", y="averageRating")
+    create_scatter(name="votes", x="numVotes", y="averageRating")
+
+
 if __name__ == "__main__":
     df = read_processed_data()
+    sampled = df.sample(1000)
     generate_hists(df)
+    generate_scatter_plots(sampled)
